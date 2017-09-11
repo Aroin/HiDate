81f35a6f183caf7fbc85e8689ba1ac8f50da765a
H4mit <h4mid.taheri@gmail.com>
2017-09-11 16:48:43 +0430

    add - demo timepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hi Datepicker :)';
+  title = 'برچیننده تقویم سلام :)';
   date: any;
 }
